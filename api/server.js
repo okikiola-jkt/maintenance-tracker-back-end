f8cf2e4b0656b2dc9296e6e8309e7120f4650799
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-09-24 23:25:16 -0600

    db.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const maintenanceTrackerRoutes = require("./src/admin/routes");
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello world");
 })
+
+app.use("/api/v1/maintenanceTracker", maintenanceTrackerRoutes);
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
