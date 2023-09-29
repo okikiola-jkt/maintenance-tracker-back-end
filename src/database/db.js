55af539321f00cf7f862d8f7ac015e93d16919bc
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-09-28 23:17:30 -0600

    Added request.rest and updates dotenv

--- src/database/db.js
+++ src/database/db.js
@@ -3,11 +3,11 @@ require ('dotenv').config();
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "maintenance_tracker",
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: 5432,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
--- src/server.js
+++ src/server.js
@@ -3,8 +3,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello world");
+const posts = [
+    {
+        username: 'Kyle',
+        password: 'Post 1'
+    },
+
+    {
+        username: 'David',
+        password: 'David2'
+    }
+]
+
+app.get('/posts', (req, res) => {
+    res.json(posts)
 })
 
 
