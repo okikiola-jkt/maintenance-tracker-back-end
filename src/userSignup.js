330bad94d83cbdda9bbaf227ea2adf34b9375ab9
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-09-30 18:53:35 -0600

    Added user sign up

---
+++ src/userSignup.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt');
+const db = require('../src/database/db')
+
+const emailExists = async (email) => {
+    const query = 'SELECT email FROM users WHERE email = $1';
+    const response = await db.query(query, [email]);
+
+    return response.rows.length > 0;
+}
+
+const registerUser = async (email, password, name) => {
+    try {
+        if (await emailExists(email)) {
+       
+            throw new Error('Email already registered');
+        } else {
+            const hashedPassword = bcrypt.hashSync(password, 10);
+            const query = 'INSERT INTO users (email, password, name) VALUES ($1, $2, $3)';
+
+            await db.query(query, [email, hashedPassword, name]);
+        }
+
+        console.log('User registration successful');
+    } catch (error) {
+        console.error('User registration error:', error.message);
+    }
+}
+
+
+const newUser = {
+    email: 'newuser@example.com',
+    password: 'newpassword',
+    name: 'new user',
+};
+
+registerUser(newUser.email, newUser.password, newUser.name);
