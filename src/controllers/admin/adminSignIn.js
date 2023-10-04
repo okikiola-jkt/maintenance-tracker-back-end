16fa1c8065664a3eb4e32edb6b888d31bf3b1cef
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-10-04 00:09:13 -0600

    Implemented admin sign-in

--- src/controllers/admin/adminSignIn.js
+++ src/controllers/admin/adminSignIn.js
@@ -1,41 +1,57 @@
 const db = require("../../database/db");
-const { password } = require("pg/lib/defaults");
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 
 const getAdminByEmail = async (email) => {
     const query = 'SELECT * FROM admin WHERE email = $1';
     const response = await db.query(query, [email]);
-
     return response.rows[0];
 };
 
-const loginAdmin = async (email, password) => {
+const loginAdmin = async (request, response) => {
     try{
+        const {email, password} = request.body;
         const admin = await getAdminByEmail(email);
+        if(!admin) {
+            return response.status(401).json({
+                status: "error",
+                message: "Invalid email or password"
+            })
+        }
         
-        if (!admin){
-            throw new Error("Admin does not exist");
-        }
-        const isPasswordValid = await bcrypt.compare(password, admin.password);
+        const isPasswordValid = await bcrypt.compare (password, admin.password)
+        if (isPasswordValid) {
+            const token = jwt.sign({
+                id: admin.id,
+                exp: Math.floor(Date.now() / 1000) + (60 * 60)
+            },process.env.SECRETKEY);
+            return response.status(200).json({
+                status: "success",
+                message: "Login successful",
+                token: token
+            });
 
-        if (!isPasswordValid){
-            throw new Error("Incorrect Password for email")
+        }else {
+            return response.status(401).json({
+                status: "error",
+                message: "Invalid email or password"
+            })
         }
-     console.log('Login Successful');
-    }
 
-    catch (error) {
-        console.log('Login Error:', error.message)
+    } catch (error) {
+        console.error(error)
+        return response.status(500).json({
+            status: "error",
+            message: "An error occured while processing your result"
+        });
     }
 
 };
 
-const adminCredentials = {
-    email: 'okiki@gmail.com',
-    password: 'okiki',
-};
+module.exports = loginAdmin;
 
-loginAdmin(adminCredentials.email, adminCredentials.password);
+
 
 
