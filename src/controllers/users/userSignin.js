5ac05f745eb4976ab8ad344c6719b505e53a1e9e
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-10-03 23:20:11 -0600

    implemented user signin

--- src/controllers/users/userSignin.js
+++ src/controllers/users/userSignin.js
@@ -1,41 +1,56 @@
 const db = require("../../database/db");
-const { password } = require("pg/lib/defaults");
+const jwt= require('jsonwebtoken')
 const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 
 const getUserByEmail = async (email) => {
-    const query = 'SELECT * FROM user WHERE email = $1';
+    const query = 'SELECT * FROM users WHERE email = $1';
     const response = await db.query(query, [email]);
-
     return response.rows[0];
 };
 
-const loginUser = async (email, password) => {
-    try{
+const loginUser = async (request, response) => {
+    try {
+        const {email, password} = request.body;
+
         const user = await getUserByEmail(email);
-        
-        if (!user){
-            throw new Error("User does not exist");
+        if (!user) {
+            return response.status(401).json({
+                status: "error",
+                message: "Invalid email or password"
+            });
         }
+
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
-        if (!isPasswordValid){
-            throw new Error("Incorrect Password for email")
+        if (isPasswordValid){
+            const token = jwt.sign({ 
+                id: user.id,
+                exp: Math.floor(Date.now() / 1000) + (60 * 60)
+            }, process.env.SECRETKEY);
+
+            return response.status(200).json({
+                status: "success",
+                message: "Login successful",
+                token: token
+            });
+        } else {
+            return response.status(401).json({
+                status: "error",
+                message: "Invalid email or password"
+            })
         }
-     console.log('Login Successful');
+    } catch (error) {
+        console.error(error);
+        return response.status(500).json({
+            status: "error",
+            message: "An error occured while processing your request"
+        });
     }
-
-    catch (error) {
-        console.log('Login Error:', error.message)
-    }
-
 };
 
-const userCredentials = {
-    email: 'okiki@gmail.com',
-    password: bcrypt.hashSync('password', 10),
-};
 
-loginUser(userCredentials.email, userCredentials.password);
+module.exports = loginUser;
 
 
