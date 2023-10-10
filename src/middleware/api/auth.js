5f241c797626c7c0134f63059d11a77b7613853c
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-10-10 12:33:27 -0600

    Made changes to auth.js

--- src/middleware/api/auth.js
+++ src/middleware/api/auth.js
@@ -1,17 +1,32 @@
 const jwt = require('jsonwebtoken');
+const db = require('../../database/db');
 
-const apiAuth = (request, response, next) => {
+
+const getUserById = async (id) => {
+    const query = 'SELECT * FROM requests WHERE id = $1'
+    const response = await db.query(query, [id]);
+    return response.rows[0];
+}
+
+const apiAuth = async (request, response, next) => {
     try{
-        const tokem = request.headers.authorization.split('')[1];
+        const token = request.headers.authorization.split('')[1];
         const decodedToken = jwt.verify(token, 'SECRETKEY');
         const id = decodedToken.Id;
-        if (request.body.id && request.body.id !== id) {
-            throw 'Invalid user ID';
-        } else {
+        
+        const user = await getUserById(id);
+        if (!user) {
+            return response.status(401).json({
+                status: "error",
+                message: "User does not exist"
+            })
+        }
+        else{
             next()
         }
+
     } catch {
-        response.status(401).json({
+        return response.status(401).json({
             error: new Error('Invalid request')
         });
     }
