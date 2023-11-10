b0c4fc33c6bf1c579dd9d30fe5e63afc0bd4367f
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-11-10 13:35:37 -0700

    New updates to port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(
@@ -20,6 +20,7 @@ app.use(routes);
 app.get('/posts', (req, res) => {
     res.json(posts);
 })
+
 
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
